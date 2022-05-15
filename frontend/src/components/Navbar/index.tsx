c699561dc5018658e07419717183b3489c67f209
Elisa Andrade <elisalvsan@gmail.com>
2022-05-14 21:13:15 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+export function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsserie-nav-content">
+                    <h1>DS/SERIE</h1>
+                    <a href="https://github.com/elisalvsan" target="_blank" rel="noreferrer">
+                        <div className="dsserie-contact-container">
+                            <GithubIcon />
+                            <p className="dsserie-contact-link">/elisalvsan</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
