dbf494d675092a5984ba21f1c4229bcdf8410fd1
Elisa Andrade <elisalvsan@gmail.com>
2022-05-16 00:07:53 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,23 @@
 import { ReactComponent as Arrow } from 'assets/img/Arrow.svg';
+import { SeriePage } from 'types/serie';
 import './styles.css';
 
+type Props = {
+    page: SeriePage;
+    onChange: Function;
+}
 
-function Pagination() {
+function Pagination({ page, onChange }: Props) {
     return (
         <div className="dsserie-pagination-container">
             <div className="dsserie-pagination-box">
-                <button className="dsserie-pagination-button" disabled={true} >
+                <button className="dsserie-pagination-button" disabled={page.first}
+                    onClick={() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="dsserie-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="dsserie-pagination-button" disabled={page.last}
+                    onClick={() => onChange(page.number + 1)} >
                     <Arrow className="dsserie-flip-horizontal" />
                 </button>
             </div>
