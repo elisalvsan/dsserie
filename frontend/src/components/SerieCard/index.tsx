d80172f2df779e6ac07ddd43450ea223d3fdaaa5
Elisa Andrade <elisalvsan@gmail.com>
2022-05-15 23:56:27 -0300

    Show score

--- frontend/src/components/SerieCard/index.tsx
+++ frontend/src/components/SerieCard/index.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 import { Serie } from "types/serie";
 
 type Props = {
-    serie : Serie;
+    serie: Serie;
 }
 
-function SerieCard({serie} : Props) {
+function SerieCard({ serie }: Props) {
 
     return (
         <div>
             <img className="dsserie-serie-card-image" src={serie.image} alt={serie.title} />
             <div className="dsserie-card-bottom-container">
                 <h3>{serie.title}</h3>
-                <SerieScore />
+                <SerieScore count={serie.count} score={serie.score} />
                 <Link to={`/form/${serie.id}`}>
                     <div className="btn btn-primary dsserie-btn">Avaliar</div>
                 </Link>
--- frontend/src/components/SerieScore/index.tsx
+++ frontend/src/components/SerieScore/index.tsx
@@ -1,15 +1,17 @@
 import SerieStars from "components/SerieStars";
 import './styles.css';
 
-function SerieScore() {
+type Props = {
+    score: number;
+    count: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+function SerieScore({score, count} : Props) {
 
     return (
         <div className="dsserie-score-container">
             <p className="dsserie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <SerieStars />
+            <SerieStars score={score} />
             <p className="dsserie-score-count">{count} avaliações</p>
         </div>
 
